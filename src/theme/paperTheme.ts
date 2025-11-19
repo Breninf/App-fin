395558296c08d4564018ccad043b13de669a67d8
Breno <felipebreno411@gmail.com>
2025-11-18 21:16:04 -0300

    feat: adicionar React Native Paper e tema de verdade

---
+++ src/theme/paperTheme.ts
@@ -0,0 +1,10 @@
+import { MD3LightTheme as DefaultTheme } from "react-native-paper";
+
+export const paperTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "#4CAF50",
+    secondary: "#FFC107",
+  },
+};
